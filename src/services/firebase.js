383012f37ed93d0fc2a521b1036bf33d7c516b04
nicolasrigourd <nico.rigourd29@gmail.com>
2026-07-02 13:53:38 -0300

    34

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  apiKey: "AIzaSyCc-QMneztyou0qbtnUnAp61ECGZtiAqjo",
+  authDomain: "webappcadeteria.firebaseapp.com",
+  projectId: "webappcadeteria",
+  storageBucket: "webappcadeteria.firebasestorage.app",
+  messagingSenderId: "918934733683",
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  databaseURL: "https://webappcadeteria-default-rtdb.firebaseio.com",
 };
 
 export const app = initializeApp(firebaseConfig);
